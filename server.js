10cc1b3359a82bd622a74f9e40d9b7a514420deb
Jamela Jefferson <JamelaAki@Jamelas-MacBook-Pro.local>
2017-08-20 11:31:26 -0500

    App Setup

---
+++ server.js
@@ -0,0 +1,36 @@
+var express = require("express");
+var exphbs = require("express-handlebars");
+var methodOverride = require('method-override')
+var bodyParser = require('body-parser')
+var app = express();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// parse an HTML body into a string 
+// app.use(bodyParser.text({ type: 'text/html' }))
+
+// app.get('/', function (req, res) {
+//   res.send('Hello World')
+// })
+
+// app.listen(3000)
